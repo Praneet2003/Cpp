927f0a36187e20be11c21115a0b64b596de52617
Praneet2003 <rajpraneet4@gmail.com>
2024-04-11 20:18:00 +0530

    While loop

---
+++ Day2/loops.cpp/do_while_loop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int index=0;
+    do{
+        cout<<"you are at index:" << index<<endl;
+        index++;
+    }while(index<35);
+    return 0;
+}
