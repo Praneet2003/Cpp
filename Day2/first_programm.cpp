19e46fd4895c79cfc6eaa4785f8625540ba0e2fa
Praneet2003 <rajpraneet4@gmail.com>
2024-04-11 20:15:22 +0530

    hello world program

---
+++ Day2/first_programm.cpp
@@ -0,0 +1,12 @@
+//Write a programm to print "hello World"!
+#include<iostream>//Pre-processor programm.
+using namespace std;
+int main(){
+    cout<<"Hello World\n"<<endl;
+    //cout is Console output, it print's to the console.
+    //endl gives the new line.
+    cout<<"this is in new line\n";//
+    // we can also have a new line by using '\n' same as we did in C programming.
+    cout<<"This is Second new line by using backsliash n.";
+    return 0;
+}
