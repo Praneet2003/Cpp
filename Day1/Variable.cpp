e3651d900347ace54cb5d949dd4171bf7c954b49
Praneet2003 <rajpraneet4@gmail.com>
2024-04-12 23:04:48 +0530

    Variables in C++

---
+++ Day1/Variable.cpp
@@ -0,0 +1,18 @@
+// Variables:-
+// Declaration of Variables:-
+// <type><variable_name>;(type can be a number,letter,test)
+// If a variable is Declared then the compiler will assign a memory location to it........
+//
+//Initialization & Declaration of Variable:-
+// <type><variable_name>=<value>;
+//Eg:- int number =9;
+
+//Write a program to take a variable X as a input from user then printf its value.
+#include<iostream>
+using namespace std;
+int main(){
+    int X;
+    cout << Enter the value of X';
+
+
+}
