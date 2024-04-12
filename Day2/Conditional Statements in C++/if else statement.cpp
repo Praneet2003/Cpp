68d6f044d96691d1233b9db751291181893c5191
Praneet2003 <rajpraneet4@gmail.com>
2024-04-12 23:04:10 +0530

    If else Statements

---
+++ Day2/Conditional Statements in C++/if else statement.cpp
@@ -0,0 +1,14 @@
+//if else Statements:-
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter Your Age:-";
+    cin>>age;
+    if(age>=18){
+        cout<<"You can vote";
+    }else{
+        cout<<"you Cannot Vote";
+    }
+    return 0;
+}
