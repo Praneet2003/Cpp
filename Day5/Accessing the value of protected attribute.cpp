13c8e5cf15372d8de8765c2187d096eee7cea424
Praneet2003 <rajpraneet4@gmail.com>
2024-04-15 23:59:07 +0530

    Accessing the protected  attribute

---
+++ Day5/Accessing the value of protected attribute.cpp
@@ -0,0 +1,30 @@
+//Acessing the value of Protected Attribute using subclass:-
+//Protected is similar to private, but it can also be accessed from inherited class.
+#include<iostream>
+using namespace std;
+// Base class
+class Employee {
+  protected: // Protected access specifier
+    int salary;
+};
+
+// Derived class
+class Programmer: public Employee {
+  public:
+    int bonus;
+    void setSalary(int s) {
+      salary = s;
+    }
+    int getSalary() {
+      return salary;
+    }
+};
+
+int main() {
+  Programmer myObj;
+  myObj.setSalary(50000);
+  myObj.bonus = 15000;
+  cout << "Salary: " << myObj.getSalary() << "\n";
+  cout << "Bonus: " << myObj.bonus << "\n";
+  return 0;
+}
